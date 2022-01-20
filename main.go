5b33501918922bd49f96dd605106ac8e4b067b3b
Joe Henke <joed.henke@gmail.com>
2022-01-20 12:58:46 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+const index = `<html>
+<h1>Joe's Music Project</h1>
+</html>`
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+	log.Println("Starting up...")
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintln(rw, index)
+	})); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Shutdown complete.")
+}
